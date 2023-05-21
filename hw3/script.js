9726c935984833dbc337822b62b26b64bad96808
Tatli <cosimadelphine@bk.ru>
2023-05-21 23:32:21 +0300

    added 1 task

---
+++ hw3/script.js
@@ -0,0 +1,12 @@
+// Задание 1
+let password = 'пароль';
+let enterPassword = prompt ('Введите пароль');
+
+if (password === 'пароль') {
+    console.log('Пароль введен верно');
+} else if (password != 'пароль') {
+    console.log('Пароль введен неправильно'); 
+}
+
+
+
